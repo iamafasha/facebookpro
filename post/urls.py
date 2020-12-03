5535010c7c157491afb794cd141cff7b2926dfe3
Isakiye Afasha <afashaisakiye@gmail>
2020-12-03 08:26:56 +0300

    empty image shown when post has no image Fixes #5

--- post/urls.py
+++ post/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/', views.profile , name="profile" ),
+    path('', views.profile , name="profile" ),
     path('add_post/',views.create_post,name="create_post"),
     path('<int:id>/', views.single_post , name="sinlge_post" ),
     path('<int:post_id>/comments/create',views.create_comment,name="add_comment"),
